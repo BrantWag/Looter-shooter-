bf57e589568fb748a16046fed6bad1eeda33b825
Brant Wagner <brant.wagner1904@gmail.com>
2020-08-20 14:37:36 -0700

    More updates
    add alot

---
+++ Assets/FPS/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
+    void Update()
+    {
+        UpdateTimerUI();
+    }
+    //call this on update
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
+    }
+
+}
